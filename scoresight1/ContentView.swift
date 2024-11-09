2e23068809a774d6d67110ad42c929e189e74dd8
Christine <christinelee263@gmail.com>
2024-11-09 11:46:59 +0800

    Initial Commit

---
+++ scoresight1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  scoresight1
+//
+//  Created by Crystal on 9/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ scoresight1/scoresight1App.swift
@@ -0,0 +1,17 @@
+//
+//  scoresight1App.swift
+//  scoresight1
+//
+//  Created by Crystal on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct scoresight1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
