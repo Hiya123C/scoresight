6c93080d9021a04c96cbdefb47093eef99b23edd
Christine <christinelee263@gmail.com>
2024-11-09 13:23:44 +0800

    added landscape

---
+++ scoresight1/SandboxView.swift
@@ -0,0 +1,18 @@
+//
+//  SandboxView.swift
+//  scoresight1
+//
+//  Created by Crystal on 9/11/24.
+//
+
+import SwiftUI
+
+struct SandboxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SandboxView()
+}
