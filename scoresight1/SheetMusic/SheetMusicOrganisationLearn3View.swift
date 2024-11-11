26b54f01657e4d71801a3ad41d4df19c37835ee7
Christine <christinelee263@gmail.com>
2024-11-11 09:11:20 +0800

    X + back + next

--- scoresight1/SheetMusic/SheetMusicOrganisationLearn3View.swift
+++ scoresight1/SheetMusic/SheetMusicOrganisationLearn3View.swift
@@ -1,15 +1,79 @@
 //
-//  SheetMusicOrganisationLearn3View.swift
+//  SheetMusicOrganisationLearn2View.swift
 //  scoresight1
 //
-//  Created by Crystal on 11/11/24.
+//  Created by Li Jiansheng on 9/11/24.
 //
 
 import SwiftUI
 
 struct SheetMusicOrganisationLearn3View: View {
+    
+    @State private var isFullScreenPresented = false
+    @State private var isFullScreenPresented2 = false
+    @State private var isFullScreenPresented3 = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Button(action:{print("hehe")
+                isFullScreenPresented = true
+            }){
+                Image(systemName: "x.circle")
+                    .symbolRenderingMode(.palette)
+                    .foregroundStyle(.black, .white)
+                    .font(.system(size:50))
+                    .position(x:0,y:50)//find out how to replace position
+            }
+            
+            HStack{
+                
+                Button(action: {
+                    isFullScreenPresented2 = true
+                })
+                {
+                    Text("back")
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.black, lineWidth: 3)
+                                .frame(width:100,height:50)
+                            
+                        )
+                        .foregroundStyle(.black)
+                        .font(.system(size: 25))
+                    Spacer()
+                    
+                }
+                Button(action: {
+                    isFullScreenPresented3 = true
+                })
+                {
+                    Text("next")
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.black, lineWidth: 3)
+                                .frame(width:100,height:50)
+                            
+                        )
+                        .foregroundStyle(.black)
+                        .font(.system(size: 25))
+                    
+                    
+                }
+            }
+            
+            
+        }
+        .fullScreenCover(isPresented: $isFullScreenPresented) {
+            SheetMusicOrganisationView()
+        }
+        .fullScreenCover(isPresented: $isFullScreenPresented2) {
+            SheetMusicOrganisationLearn2View()
+        }
+//        .fullScreenCover(isPresented: $isFullScreenPresented3) {
+//            SheetMusicOrganisationLearn4View()
+//        }
     }
 }
 
--- scoresight1/SheetMusic/SheetMusicOrganisationLearnView.swift
+++ scoresight1/SheetMusic/SheetMusicOrganisationLearnView.swift
@@ -1,15 +1,72 @@
-//
-//  SheetMusicOrganisationLearn2View.swift
-//  scoresight1
-//
-//  Created by Li Jiansheng on 9/11/24.
-//
+//possible to move to next view without transition animation?
 
 import SwiftUI
 
 struct SheetMusicOrganisationLearnView: View {
+    
+    @State private var isFullScreenPresented = false
+    @State private var isFullScreenPresented2 = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Button(action:{
+                print("haha")//same issue
+                isFullScreenPresented = true
+            }){
+                Image(systemName: "x.circle")
+                    .symbolRenderingMode(.palette)
+                    .foregroundStyle(.black, .white)
+                    .font(.system(size:50))
+                    .position(x:0,y:50)//find out how to replace position
+            }
+            HStack(alignment: .center){ //the photo no movee.
+                Image("bar")
+                    .resizable()
+                    .frame(width: 500, height: 250, alignment: .bottom)
+                    .position(x:300,y:70)
+                
+                 //position?
+                VStack{
+                    Text("this is a ")
+                        .font(.system(size: 40))
+                    Text("bar")
+                        .font(.system(size:80))
+                        .bold()
+
+                }
+            }
+            
+            
+            HStack{
+                Spacer()
+                Button(action: {
+                    isFullScreenPresented2 = true
+                })
+                {
+                    Text("next")
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.black, lineWidth: 3)
+                                .frame(width:100,height:50)
+                            
+                        )
+                        .foregroundStyle(.black)
+                        .font(.system(size: 25))
+                    
+                    
+                }
+            }
+            
+            
+        }
+        .fullScreenCover(isPresented: $isFullScreenPresented) {
+            SheetMusicOrganisationView()
+        }
+        .fullScreenCover(isPresented: $isFullScreenPresented2) {
+            SheetMusicOrganisationLearn2View()
+        }
+        
     }
 }
 
