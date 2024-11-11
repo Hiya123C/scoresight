721f6395876e0ac6cd5a510ac881dd0fd2f7c442
Christine <christinelee263@gmail.com>
2024-11-11 09:11:02 +0800

    X button + back + next

---
+++ scoresight1/Sandbox/Sandbox3View.swift
@@ -0,0 +1,18 @@
+//
+//  Sandbox3View.swift
+//  scoresight1
+//
+//  Created by Crystal on 10/11/24.
+//
+
+import SwiftUI
+
+struct Sandbox3View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Sandbox3View()
+}
---
+++ scoresight1/SheetMusic/SheetMusicOrganisationLearn2View.swift
@@ -0,0 +1,18 @@
+//
+//  SheetMusicOrganisationLearn2View.swift
+//  scoresight1
+//
+//  Created by Crystal on 10/11/24.
+//
+
+import SwiftUI
+
+struct SheetMusicOrganisationLearn2View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SheetMusicOrganisationLearn2View()
+}
