fd6ad3bdc53e24bad5495440945cedb6af28018a
avrilwl <alex410w3asl3y@gmail.com>
2024-11-16 14:52:18 +0800

    added padding in btw dynamics next button and speaker

--- scoresight1/Dynamics/DynamicsLearn5View.swift
+++ scoresight1/Dynamics/DynamicsLearn5View.swift
@@ -47,15 +47,17 @@ struct DynamicsLearn5View: View {
                             )
                             .foregroundStyle(.black)
                             .font(.system(size: 25))
-                        Spacer()
-                        Button(action: {
-                            replayAudio()
-                        }) {
-                            Image(systemName: "speaker.wave.2.fill")
-                                .font(.system(size: 30))
-                                .foregroundStyle(.black)
-                        }
+                        
                     }
+                    Spacer()
+                    Button(action: {
+                        replayAudio()
+                    }) {
+                        Image(systemName: "speaker.wave.2.fill")
+                            .font(.system(size: 30))
+                            .foregroundStyle(.black)
+                    }
+                    .padding()
                     NavigationLink{
                         DynamicsLearn6View()
                     } label:{
